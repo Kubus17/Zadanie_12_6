1cc8ee70418138f5fab4661a110aefe19f7c0e6d
Piotr-PLWRAK2a2\Piotr <kubus-o_2@tlen.pl>
2017-08-02 07:18:26 +0200

    Poprawiłem oscylowanie w pliku style.css oraz dodałem nazwy w scrips.js

--- js/scripts.js
+++ js/scripts.js
@@ -15,8 +15,17 @@ function showCountriesList(resp) {
     countriesList.empty();
     resp.forEach(function (item) {
         resp.forEach(function (item) {
-            $('<li><h2>').text(item.name).appendTo(countriesList);
+            $("ul").append("<h4>NAME: </h4>");
+            $('<li>').text(item.name).appendTo(countriesList);
+            $("ul").append("<h4>CAPITAL: </h4>");
             $('<li>').text(item.capital).appendTo(countriesList);
+            $("ul").append("<h4>POPULATION: </h4>");
+            $('<li>').text(item.population).appendTo(countriesList);
+            $("ul").append("<h4>BORDERS: </h4>");
+            $('<li>').text(item.borders).appendTo(countriesList);
+            $("ul").append("<h4>SUBREGION: </h4>");
+            $('<li>').text(item.subregion).appendTo(countriesList);
+            $("ul").append("<h4>REGION: </h4>");
             $('<li>').text(item.region).appendTo(countriesList);
         });
     });
